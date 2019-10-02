b3ecf064246958df9e2d9b41de70453fa7a6736f
Aidan Maycock <aidan.dexter@icloud.com>
2019-10-02 16:25:22 -0400

    use :show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session, notice: "Logged in!"
+      redirect_to :show, notice: "Logged in!"
     else
       render "new", notice: "Email or password is invalid"
     end
