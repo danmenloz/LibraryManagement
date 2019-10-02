6659c669f20163e0d699323a511d1055d6e14882
Aidan Maycock <aidan.dexter@icloud.com>
2019-10-02 15:23:49 -0400

    Fix notice display bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to 'Show', notice: "Logged in!"
     else
-      notice: "Email or password is invalid"
-      render "new"
+      render "new", notice: "Email or password is invalid"
     end
   end
 
