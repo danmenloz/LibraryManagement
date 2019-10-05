c8881f98e3c792eeb46c7f4128b7c4edc07cb4e6
drao <you@example.com>
2019-10-05 14:48:58 -0400

    commit :

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,17 +7,9 @@ class Book < ActiveRecord::Base
   validates :isbn, :uniqueness => true
   validates :edition, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :copies, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-<<<<<<< HEAD
-  def self.search(search)
-    if search
-      where('title LIKE ?', "%#{search}%") || where('isbn LIKE ?', "%#{search}%")
-    end
-  end
-=======
+
 
   def self.search(search)
     where("title LIKE ?", "%#{search}%")
   end
-
->>>>>>> fe7bf1f59bab8ed5235d3fd4cce1ce97a400eea2
 end
