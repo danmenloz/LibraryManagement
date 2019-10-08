7c67eee8448bc22e6089023b297bc47eb943b3bf
Aidan Maycock <aidan.dexter@icloud.com>
2019-10-08 15:05:30 -0400

    Add migration files

---
+++ db/migrate/20190925031856_create_libraries.rb
@@ -0,0 +1,15 @@
+class CreateLibraries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :libraries do |t|
+      t.string :name
+      t.string :university
+      t.string :location
+      t.integer :max_days
+      t.float :overdue_fines
+      # t.string :caller_locations
+
+
+      t.timestamps
+    end
+  end
+end
