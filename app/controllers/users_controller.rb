45db42356285f326d7bcf73dba1a8c3ec6d60c68
Aidan Maycock <aidan.dexter@icloud.com>
2019-10-09 16:29:25 -0400

    make current user acessible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
+    @current_user = current_user # call helper method
   end
 
   # POST /users
